2e6f0153a7bb006f3c49caf1e319bf2735d9cae6
ayush <ayushkumar605575@gmail.com>
2024-04-07 18:02:31 +0530

    To be implemented

---
+++ google_drive_data.py
@@ -0,0 +1,155 @@
+import os.path
+import pickle, tqdm, re, requests
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
+          'https://www.googleapis.com/auth/drive',
+          'https://www.googleapis.com/auth/drive.file']
+
+
+def get_drive_access():
+  """Shows basic usage of the Drive v3 API.
+  Prints the names and ids of the first 10 files the user has access to.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("drive", "v3", credentials=creds)
+
+    # Call the Drive v3 API
+    results = (
+        service.files()
+        .list(pageSize=10, fields="nextPageToken, files(id, name)")
+        .execute()
+    )
+    items = results.get("files", [])
+
+    if not items:
+      print("No files found.")
+      return
+    print("Files:")
+    for item in items:
+      print(f"{item['name']} ({item['id']})")
+  except HttpError as error:
+    # TODO(developer) - Handle errors from drive API.
+    print(f"An error occurred: {error}")
+  return build('drive', 'v3', credentials=creds)
+
+def get_gdrive_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    # return Google Drive API service
+    return build('drive', 'v3', credentials=creds)
+    
+def search(service, query):
+    # search for the file
+    result = []
+    page_token = None
+    while True:
+        response = service.files().list(q=query,
+                                        spaces="drive",
+                                        fields="nextPageToken, files(id, name, mimeType)",
+                                        pageToken=page_token).execute()
+        # iterate over filtered files
+        for file in response.get("files", []):
+            result.append((file["id"], file["name"], file["mimeType"]))
+        page_token = response.get('nextPageToken', None)
+        if not page_token:
+            # no more files
+            break
+    return result
+
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+        # get the file size from Content-length response header
+        file_size = int(response.headers.get("Content-Length", 0))
+        # extract Content disposition from response headers
+        content_disposition = response.headers.get("content-disposition")
+        # parse filename
+        filename = re.findall("filename=\"(.+)\"", content_disposition)[0]
+        print("[+] File size:", file_size)
+        print("[+] File name:", filename)
+        progress = tqdm.tqdm(response.iter_content(CHUNK_SIZE), f"Downloading {filename}", total=file_size, unit="Byte", unit_scale=True, unit_divisor=1024)
+        with open(destination, "wb") as f:
+            for chunk in progress:
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    # update the progress bar
+                    progress.update(len(chunk))
+        progress.close()
+
+    # base URL for download
+    URL = "https://docs.google.com/uc?export=download"
+    # init a HTTP session
+    session = requests.Session()
+    # make a request
+    response = session.get(URL, params = {'id': id}, stream=True)
+    print("[+] Downloading", response.url)
+    # get confirmation token
+    token = get_confirm_token(response)
+    if token:
+        params = {'id': id, 'confirm':token}
+        response = session.get(URL, params=params, stream=True)
+    # download to disk
+    save_response_content(response, destination)  
+
+def download():
+    service = get_gdrive_service()
+    # the name of the file you want to download from Google Drive 
+    filename = "self.jpg"
+    # search for the file by name
+    search_result = search(service, query=f"name='{filename}'")
+    # get the GDrive ID of the file
+    file_id = search_result[0][0]
+    # make it shareable
+    service.permissions().create(body={"role": "reader", "type": "anyone"}, fileId=file_id).execute()
+    # download file
+    download_file_from_google_drive(file_id, filename)
+
+if __name__ == "__main__":
+    download()
